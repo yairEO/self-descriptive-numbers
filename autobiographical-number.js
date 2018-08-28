6b3db780987ea86a7381677e4ab3a8bced657ee4
vsync <vsync.design@gmail.com>
2018-08-28 21:40:09 +0300

    -

--- autobiographical-number.js
+++ autobiographical-number.js
@@ -1,21 +1,28 @@
 function createNumber(N){
-	var result = Array(N).fill(0),
+    var result = Array.from({length:N}, () => Math.floor(Math.random() * 10)),
+        memory = [],
         count;
 
-	function countInArray(array, value){
-		return array.reduce((a, b, idx) => a + (b === value && idx != value), 0);
-	}
+    function countInArray(array, value){
+        return array.reduce((a, b, idx) => a + (b === value && idx != value), 0);
+    }
 
-	for( let i = 0; i < result.length; i++ ){
-		count = countInArray(result, i);
+    for( let i = 0; i < result.length; i++ ){
+        count = countInArray(result, i);
 
-		// if the digit on index [i] should change, then update the result
-		if( result[i] != count ){
-			result[i] = count;
-			if( i == result.length-1 ) // if last item changed, restart the loop
-				i = -1;
-		}
-	}
+        if( count > 9 )
+            return NaN;
 
-	return result;
+        // if the digit on index [i] should change, then update the result
+        if( result[i] != count )
+            result[i] = count;
+
+        // if reached the end of the iteration
+        if( i == result.length-1 && !memory.includes( result.join('') ) ){
+            i = -1;
+            memory.push( result.join('') );
+        }
+    }
+
+    return +(result.join(''));
 }
